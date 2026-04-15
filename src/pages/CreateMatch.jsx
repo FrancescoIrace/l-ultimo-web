1f456b0920f746fd4fcc687359243b4c722a9fd4
FrancescoIrace <iracefrancesco99@outlook.com>
2026-04-15 20:36:48 +0200

    Add altre opzioni "sport" + Add campo "description"

--- src/pages/CreateMatch.jsx
+++ src/pages/CreateMatch.jsx
@@ -61,15 +61,16 @@ export default function CreateMatch() {
     return (
         <div className="max-w-md mx-auto p-6 bg-white min-h-screen">
             <button
-                    onClick={() => navigate('/')}
-                    type="button"
-                    className="w-30 h-5 text-xs cursor-pointer flex items-center justify-center bg-red-600 text-white py-4 mb-4 rounded-2xl font-bold shadow-md shadow-red-200 hover:bg-red-700 transition-all active:scale-95 disabled:opacity-50"
-                >
-                    TORNA INDIETRO
-                </button>
+                onClick={() => navigate('/')}
+                type="button"
+                className="w-30 h-5 text-xs cursor-pointer flex items-center justify-center bg-red-600 text-white py-4 mb-4 rounded-2xl font-bold shadow-md shadow-red-200 hover:bg-red-700 transition-all active:scale-95 disabled:opacity-50"
+            >
+                TORNA INDIETRO
+            </button>
             <h2 className="text-2xl font-black text-slate-800 mb-6 uppercase">Organizza Match</h2>
 
             <form onSubmit={handleSubmit} className="space-y-5">
+                {/* SPORT */}
                 <div>
                     <label className="block text-xs font-bold text-slate-500 uppercase mb-1">Sport</label>
                     <select
@@ -78,13 +79,18 @@ export default function CreateMatch() {
                         onChange={(e) => setFormData({ ...formData, sport: e.target.value })}
                     >
                         <option>Calcetto</option>
+                        <option>Calcio a 7</option>
+                        <option>Calcio a 11</option>
                         <option>Padel</option>
-                        <option>Basket</option>
-                        <option>Tennis</option>
+                        <option>Basket (3vs3)</option>
+                        <option>Basket (5vs5)</option>
+                        <option>Tennis singolo</option>
+                        <option>Tennis doppio</option>
                         <option>Volley</option>
                     </select>
                 </div>
 
+                {/* TITOLO */}
                 <div>
                     <label className="block text-xs font-bold text-slate-500 uppercase mb-1">Titolo (Opzionale)</label>
                     <input
@@ -95,6 +101,7 @@ export default function CreateMatch() {
                     />
                 </div>
 
+                {/* DATA E GIOCATORI */}
                 <div className="grid grid-cols-2 gap-4">
                     <div>
                         <label className="block text-xs font-bold text-slate-500 uppercase mb-1">Quando</label>
@@ -117,7 +124,8 @@ export default function CreateMatch() {
                         />
                     </div>
                 </div>
-
+ 
+                {/* LUOGO E DESCRIZIONE */}
                 <div>
                     <label className="block text-xs font-bold text-slate-500 uppercase mb-1">Dove</label>
                     <input
@@ -129,13 +137,23 @@ export default function CreateMatch() {
                     />
                 </div>
 
+                <div>
+                    <label className="block text-xs font-bold text-slate-500 uppercase mb-1">Descrizione <i className="text-slate-400 text-[10px]">(max 300 caratteri)</i></label>
+                    <textarea
+                        placeholder="Descrizione della partita"
+                        maxLength={300}
+                        className="w-full h-50 resize-none p-3 bg-slate-50 border border-slate-200 rounded-xl outline-none focus:ring-2 focus:ring-blue-500"
+                        onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                    />
+                </div>
+
                 <button
                     disabled={loading}
                     className="w-full cursor-pointer bg-blue-600 text-white py-4 rounded-2xl font-bold shadow-lg shadow-blue-200 hover:bg-blue-700 transition-all active:scale-95 disabled:opacity-50"
                 >
                     {loading ? 'Creazione in corso...' : 'PUBBLICA PARTITA'}
                 </button>
-                
+
             </form>
         </div>
     );

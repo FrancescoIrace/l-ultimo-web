44f9d8620cf1639cb366bdc14b6e484602f55b39
FrancescoIrace <iracefrancesco99@outlook.com>
2026-04-21 01:16:21 +0200

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
-  },
+    middlewares: [
+      (req, res, next) => {
+        // Se la richiesta non è un file statico e non è .json/.js/.css etc
+        // allora fai il fallback a index.html per il routing SPA
+        if (req.url === '/' || (!req.url.includes('.') && !req.url.startsWith('/api'))) {
+          req.url = '/index.html'
+        }
+        next()
+      }
+    ]
+  }
 })
